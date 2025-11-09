13bf5fb79ffb636abe8cf3f63a5d4d8b9e10a5b0
Tim <chegekennedyee@gmail.com>
2025-11-09 07:22:05 +0300

    Add basic_operations.py: simple arithmetic exercises

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# basic_operations.py
+number1 = 10
+number2 = 5
+
+# Perform operations
+addition = number1 + number2
+subtraction = number1 - number2  # number1 minus number2
+multiplication = number1 * number2
+
+# Print results in the required format
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
